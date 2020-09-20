f182b5a45068f9547cffb866916aa85827a5a9cd
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-09-20 09:06:07 +0530

    adding post mutations

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -34,7 +34,7 @@ module.exports = {
         throw new UserInputError("User not found", { errors });
       }
 
-      const match = await bcrypt.compare(password, user, password);
+      const match = await bcrypt.compare(password, user.password);
       if (!match) {
         errors.general = "Wrong credentials";
         throw new UserInputError("Wrong credentials", { errors });
