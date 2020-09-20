221f629d8b3c62865c9a8795a46a0f6e8ab85fd3
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-09-20 11:24:16 +0530

    fixing likeCount and commentCount typedef error

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,15 +4,22 @@ const commentsResolvers = require("./comments");
 const likesResolvers = require("./likes");
 
 module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
+
   Query: {
     ...postsResolvers.Query,
   },
+
   Mutation: {
     ...usersResolvers.Mutation,
     ...postsResolvers.Mutation,
     ...commentsResolvers.Mutation,
     ...likesResolvers.Mutation,
   },
+
   Subscription: {
     ...postsResolvers.Subscription,
   },
